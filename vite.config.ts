a408e76de7025142fd928e188e119a75248e0489
Jose De Avila <josedeavila@Joses-MacBook-Air.local>
2023-05-27 12:52:11 -0500

    :recycle: add aliases for routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/constants') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      { find: '@types', replacement: path.resolve(__dirname, 'src/types') },
+    ]
+  }
 })
